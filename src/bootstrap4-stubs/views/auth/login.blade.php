a902685748749d1e46d77c4ea47368ae092adf5e
Rahmani Saif El Moulouk <rsm23@users.noreply.github.com>
2017-08-24 01:24:19 +0100

    Updating login.blade.php
    removing deprecated panel-body class

--- src/bootstrap4-stubs/views/auth/login.blade.php
+++ src/bootstrap4-stubs/views/auth/login.blade.php
@@ -8,61 +8,59 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title text-center">Login</h4>
-                        <div class="panel-body">
-                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                                {!! csrf_field() !!}
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                            {!! csrf_field() !!}
 
-                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                    <label class="col-md-4 control-label">E-Mail Address</label>
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">E-Mail Address</label>
 
-                                    <div>
-                                        <input type="email" class="form-control" name="email"
-                                               value="{{ old('email') }}">
+                                <div>
+                                    <input type="email" class="form-control" name="email"
+                                           value="{{ old('email') }}">
 
-                                        @if ($errors->has('email'))
-                                            <span class="help-block">
+                                    @if ($errors->has('email'))
+                                        <span class="help-block">
                                                             <strong>{{ $errors->first('email') }}</strong>
                                                         </span>
-                                        @endif
-                                    </div>
+                                    @endif
                                 </div>
+                            </div>
 
-                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                    <label class="col-md-4 control-label">Password</label>
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Password</label>
 
-                                    <div>
-                                        <input type="password" class="form-control" name="password">
+                                <div>
+                                    <input type="password" class="form-control" name="password">
 
-                                        @if ($errors->has('password'))
-                                            <span class="help-block">
+                                    @if ($errors->has('password'))
+                                        <span class="help-block">
                                                             <strong>{{ $errors->first('password') }}</strong>
                                                         </span>
-                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox" name="remember"> Remember Me
+                                        </label>
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="form-group">
-                                    <div>
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox" name="remember"> Remember Me
-                                            </label>
-                                        </div>
-                                    </div>
+                            <div class="form-group">
+                                <div>
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-btn fa-sign-in"></i>Login
+                                    </button>
+
+                                    <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your
+                                        Password?</a>
                                 </div>
-
-                                <div class="form-group">
-                                    <div>
-                                        <button type="submit" class="btn btn-primary">
-                                            <i class="fa fa-btn fa-sign-in"></i>Login
-                                        </button>
-
-                                        <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your
-                                            Password?</a>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
