30a7c3ca697272aef0fd4a1d9813e9acde5ca95d
Rahmani Saif El Moulouk <rsm23@users.noreply.github.com>
2017-08-24 01:48:49 +0100

    Updating reset.blade.php
    Fixing styling issues

--- src/bootstrap4-stubs/views/auth/passwords/reset.blade.php
+++ src/bootstrap4-stubs/views/auth/passwords/reset.blade.php
@@ -1,79 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row justify-content-md-center mt-5">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Reset Password</h4>
 
-<div class="container">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/password/reset') }}">
+                            {!! csrf_field() !!}
 
-    <div class="row">
+                            <input type="hidden" name="token" value="{{ $token }}">
 
-        <div class="form-container small-6 small-centered columns">
+                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">E-Mail Address</label>
 
-            <div class="status_message">
-                @if (session('status'))
-                    <div class="callout">
-                        {{ session('status') }}
+                                <div class="col-md-12">
+                                    <input type="email" class="form-control" name="email"
+                                           value="{{ $email or old('email') }}">
+
+                                    @if ($errors->has('email'))
+                                        <span class="form-text text-muted">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-12">
+                                    <input type="password" class="form-control" name="password">
+
+                                    @if ($errors->has('password'))
+                                        <span class="form-text text-muted">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                                <label class="col-md-4 control-label">Confirm Password</label>
+                                <div class="col-md-12">
+                                    <input type="password" class="form-control" name="password_confirmation">
+
+                                    @if ($errors->has('password_confirmation'))
+                                        <span class="form-text text-muted">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        <i class="fa fa-btn fa-refresh"></i>Reset Password
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                @endif
+                </div>
             </div>
-
-            <div class="form-title text-center">
-                Reset Password
-            </div>
-
-            <form class="password-reset-form" method="POST" action="{{ route('password.request') }}">
-
-                {{ csrf_field() }}
-
-                <input type="hidden" name="token" value="{{ $token }}">
-
-                <div class="email">
-                    <label for="email">E-Mail Address</label>
-
-                    <input id="email" type="email" name="email" value="{{ old('email') }}" aria-describedby="emailHelpText" required autofocus>
-
-                    @if ($errors->has('email'))
-                        <span class="help-text" id="emailHelpText">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                </div>
-
-                <div class="password">
-                    <label for="password">Password</label>
-
-                    <input id="password" type="password" name="password" aria-describedby="passwordHelpText" required>
-
-                    @if ($errors->has('password'))
-                        <span class="help-text" id="passwordHelpText">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-
-                <div class="password">
-                    <label for="password-confirm">Confirm Password</label>
-
-                    <input id="password-confirm" type="password" name="password_confirmation" aria-describedby="passwordConfirmHelpText" required>
-
-                    @if ($errors->has('password'))
-                        <span class="help-text" id="passwordConfirmHelpText">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-
-                <div class="reset-button">
-                    <button type="submit" class="button">
-                        Reset Password
-                    </button>
-                </div>
-
-            </form>
-
         </div>
-
-    </div>
-
-</div>
-
 @endsection
