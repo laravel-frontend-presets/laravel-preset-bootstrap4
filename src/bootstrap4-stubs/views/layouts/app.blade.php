b6ee52623a2244dc8c122cf53965615be5532d82
Rahmani Saif El Moulouk <rsm23@users.noreply.github.com>
2017-08-24 01:22:53 +0100

    Updating app.blade.php
    Fixing all errors and wrong classes and navbar.

--- src/bootstrap4-stubs/views/layouts/app.blade.php
+++ src/bootstrap4-stubs/views/layouts/app.blade.php
@@ -14,53 +14,52 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
+<div id="app">
 
-        {{-- top bar  --}}
-        <div class="top-bar">
+    {{-- top bar  --}}
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-          <div class="top-bar-left">
-            <ul class="dropdown menu" data-dropdown-menu>
-              <li class="menu-text">{{ config('app.name', 'Laravel') }} {{ app()->version() }}</li>
-            </ul>
-          </div>
+        <a class="navbar-brand" href="#">{{ config('app.name', 'Laravel') }} {{ app()->version() }}</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+        </ul>
 
-          <div class="top-bar-right">
-            <ul class="menu">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav">
                 @if (Auth::guest())
-                    <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Login</a></li>
+                    <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">Register</a></li>
                 @else
-                    <ul class="dropdown menu" data-dropdown-menu>
-                        <li>
-                            <a href="#">{{ Auth::user()->name }}</a>
-                            <ul class="menu">
-                                <li>
-                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a href="{{ route('logout') }}" class="dropdown-item"
+                               onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                  style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </li>
                 @endif
             </ul>
-          </div>
-
         </div>
 
-        @yield('content')
+    </nav>
 
-    </div>
+    @yield('content')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script>
-        $(document).foundation();
-    </script>
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
