7bf4b48a734bc888098568bfa03a43c30442eceb
Rahmani Saif El Moulouk <rsm23@users.noreply.github.com>
2017-08-29 14:35:45 +0100

    Adding Tether to the node packages
    Adding Tether to the node packages

--- src/Bootstrap4Preset.php
+++ src/Bootstrap4Preset.php
@@ -43,6 +43,7 @@ class Bootstrap4Preset extends Preset
         return [
             'bootstrap' => '^4.0.0-beta',
             'jquery' => '^3.2.1',
+            'tether' => '^1.4.0',
             'popper.js' => '^1.11.0',
             'precss' => '^2.0.0',
         ] + Arr::except($packages, ['foundation-sites', 'bootstrap-sass', 'bulma', 'uikit']);
