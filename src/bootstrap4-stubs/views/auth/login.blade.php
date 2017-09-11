a635d5177a43d7b1f763803c1487f194c6026c50
Patrick Fruh <Kaeltis@users.noreply.github.com>
2017-09-11 13:18:15 +0200

    add col-md-12 to form divs

--- src/bootstrap4-stubs/views/auth/login.blade.php
+++ src/bootstrap4-stubs/views/auth/login.blade.php
@@ -29,7 +29,7 @@
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <label class="col-md-4 control-label">Password</label>
 
-                                <div>
+                                <div class="col-md-12">
                                     <input type="password" class="form-control" name="password">
 
                                     @if ($errors->has('password'))
@@ -41,7 +41,7 @@
                             </div>
 
                             <div class="form-group">
-                                <div>
+                                <div class="col-md-12">
                                     <div class="checkbox">
                                         <label>
                                             <input type="checkbox" name="remember"> Remember Me
@@ -51,7 +51,7 @@
                             </div>
 
                             <div class="form-group">
-                                <div>
+                                <div class="col-md-12">
                                     <button type="submit" class="btn btn-primary">
                                         <i class="fa fa-btn fa-sign-in"></i>Login
                                     </button>
